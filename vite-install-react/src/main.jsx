3ff6b303f3f981a051d4f0b3d68c2824bae334f2
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-07-04 23:45:29 +0300

    Main prettier

--- vite-install-react/src/main.jsx
+++ vite-install-react/src/main.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App/>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
