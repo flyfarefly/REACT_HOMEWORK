6b88dce376ce16d2fcf13d3d0cec7d088776c39c
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-07-04 13:06:06 +0300

    HW_79 Complete

--- vite-install-react/src/App.jsx
+++ vite-install-react/src/App.jsx
@@ -1,1 +1,16 @@
+import Definitions from "./components/Definitions.jsx";
+
+function App() {
+    const definitions = [
+        {dt: 'one', dd: 'two', id: 1},
+        {dt: `another term`, dd: `another description`, id: 2},
+    ];
+    return (
+        <>
+            <Definitions data={definitions}/>
+        </>
+    )
+}
+
+export default App
 
---
+++ vite-install-react/src/components/Definitions.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Definitions extends React.Component {
+    render() {
+        const {data} = this.props;
+        return (
+            <dl>
+                {data.map(definitions =><>
+                    <dt key={definitions.id}>{definitions.dt}</dt>
+                    <dd>{definitions.dd}</dd>
+                </>)}
+            </dl>
+        )
+    }
+}
+
+export default Definitions;
