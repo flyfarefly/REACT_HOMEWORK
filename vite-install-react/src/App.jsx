7e2d158e25fb5bdca68c1e207ca8be57b83f99ac
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-07-13 15:55:34 +0300

    HW_83 Bootstrap type

--- vite-install-react/src/App.jsx
+++ vite-install-react/src/App.jsx
@@ -1,9 +1,13 @@
 import Alert from './components/Alert.jsx';
+import React from 'react';
 
 function App() {
   return (
     <>
-      <Alert></Alert>
+      <h1>Bootstrap Alert</h1>
+      <Alert type="danger">
+        <h2 className="alert heading">what is love?</h2>
+      </Alert>
     </>
   );
 }
--- vite-install-react/src/components/Alert.jsx
+++ vite-install-react/src/components/Alert.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 class Alert extends React.Component {
+  static defaultProps = {
+    type: null
+  };
+
   render() {
+    const { type, children } = this.props;
     return (
-      <div className="alert alert-warning" role="alert">
-        what is love?
+      <div className="row">
+        {type && (
+          <div className={'alert alert-' + type} role="alert">
+            {children}
+          </div>
+        )}
       </div>
     );
   }
